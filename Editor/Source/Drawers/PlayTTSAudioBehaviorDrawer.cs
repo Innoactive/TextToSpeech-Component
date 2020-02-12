82a9e17203cee55f488c438297af737d28fa943a
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-02-12 12:11:36 +0100

    chg: The Localization submodule was moved back to core.
    add: TextToSpeechMenuItem was moved from core.
    add: Drawers for AudioData and PlayAudioBehavior modify values for behaviors PlayAudio and PlayTTSAudio.
    add:  TextToSpeechJsonTrainingSerializerTests was introduced.

---
+++ Editor/Source/Drawers/PlayTTSAudioBehaviorDrawer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using Innoactive.Hub.Training.Audio;
+using Innoactive.Hub.Training.Behaviors;
+
+namespace Innoactive.Hub.Training.Editors.Drawers
+{
+    /// <summary>
+    /// Default drawer for <see cref="PlayAudioBehavior"/>. It changes displayed name to "Play TTS Audio" or "Play Audio File", depending on which AudioData is used.
+    /// </summary>
+    [DefaultTrainingDrawer(typeof(PlayAudioBehavior.EntityData))]
+    public class PlayTTSAudioBehaviorDrawer : PlayAudioBehaviorDrawer
+    {
+        /// <inheritdoc />
+        protected override GUIContent GetTypeNameLabel(object value, Type declaredType)
+        {
+            PlayAudioBehavior.EntityData behavior = value as PlayAudioBehavior.EntityData;
+
+            if (behavior == null)
+            {
+                return base.GetTypeNameLabel(value, declaredType);
+            }
+
+            if (behavior.AudioData is TextToSpeechAudio)
+            {
+                return new GUIContent("Play TTS Audio");
+            }
+
+            if (behavior.AudioData is ResourceAudio)
+            {
+                return new GUIContent("Play Audio File");
+            }
+
+            return base.GetTypeNameLabel(value, declaredType);
+        }
+    }
+}
+
