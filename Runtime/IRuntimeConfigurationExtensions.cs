a0f375e14601de6496512d1657dd0898d7b85da1
TomWim <samo.miw@gmail.com>
2020-08-05 10:07:13 +0200

    fix: obsolete

--- Runtime/IRuntimeConfigurationExtensions.cs
+++ Runtime/IRuntimeConfigurationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Innoactive.Creator.Core.Configuration;
+﻿using System;
+using Innoactive.Creator.Core.Configuration;
 
 namespace Innoactive.Creator.TextToSpeech
 {
@@ -26,9 +27,32 @@ namespace Innoactive.Creator.TextToSpeech
         }
         
         /// <summary>
+        /// Return loaded <see cref="TextToSpeechConfiguration"/>.
+        /// </summary>
+        [Obsolete("To be more flexible with the Creator development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
+        public static TextToSpeechConfiguration GetTextToSpeechConfiguration(this IRuntimeConfiguration runtimeConfiguration)
+        {
+            if (textToSpeechConfiguration == null)
+            {
+                textToSpeechConfiguration = TextToSpeechConfiguration.LoadConfiguration();
+            }
+            
+            return textToSpeechConfiguration;
+        }
+        
+        /// <summary>
         /// Loads a new <see cref="TextToSpeechConfiguration"/>
         /// </summary>
         public static void SetTextToSpeechConfiguration(this BaseRuntimeConfiguration runtimeConfiguration, TextToSpeechConfiguration ttsConfiguration)
+        {
+            textToSpeechConfiguration = ttsConfiguration;
+        }
+        
+        /// <summary>
+        /// Loads a new <see cref="TextToSpeechConfiguration"/>
+        /// </summary>
+        [Obsolete("To be more flexible with the Creator development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
+        public static void SetTextToSpeechConfiguration(this IRuntimeConfiguration runtimeConfiguration, TextToSpeechConfiguration ttsConfiguration)
         {
             textToSpeechConfiguration = ttsConfiguration;
         }
