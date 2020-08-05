f530dbef82be2c7ac5d31c266d2f49cc7f957320
TomWim <samo.miw@gmail.com>
2020-08-05 14:36:17 +0200

    feat: rename

--- Runtime/RuntimeConfigurationExtensions.cs
+++ Runtime/RuntimeConfigurationExtensions.cs
@@ -1,38 +1,35 @@
-﻿using System;
-using Innoactive.Creator.Core.Configuration;
+﻿using Innoactive.Creator.Core.Configuration;
 
 namespace Innoactive.Creator.TextToSpeech
 {
     /// <summary>
     /// TextToSpeech extensions methods for <see cref="IRuntimeConfiguration"/>.
     /// </summary>
-    [Obsolete("This class is obsolete and replaced by RuntimeConfigurationExtensions.")]
-    public static class IRuntimeConfigurationExtensions
+    public static class RuntimeConfigurationExtensions
     {
         /// <summary>
         /// Text to speech configuration.
         /// </summary>
         private static TextToSpeechConfiguration textToSpeechConfiguration;
-        
+
         /// <summary>
         /// Return loaded <see cref="TextToSpeechConfiguration"/>.
         /// </summary>
-        [Obsolete("To be more flexible with the Creator development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
-        public static TextToSpeechConfiguration GetTextToSpeechConfiguration(this IRuntimeConfiguration runtimeConfiguration)
+        public static TextToSpeechConfiguration GetTextToSpeechConfiguration(this BaseRuntimeConfiguration runtimeConfiguration)
         {
             if (textToSpeechConfiguration == null)
             {
                 textToSpeechConfiguration = TextToSpeechConfiguration.LoadConfiguration();
             }
-            
+
             return textToSpeechConfiguration;
         }
-        
+
         /// <summary>
         /// Loads a new <see cref="TextToSpeechConfiguration"/>
         /// </summary>
-        [Obsolete("To be more flexible with the Creator development we switched to an abstract class as configuration base, consider using BaseRuntimeConfiguration.")]
-        public static void SetTextToSpeechConfiguration(this IRuntimeConfiguration runtimeConfiguration, TextToSpeechConfiguration ttsConfiguration)
+        public static void SetTextToSpeechConfiguration(this BaseRuntimeConfiguration runtimeConfiguration,
+            TextToSpeechConfiguration ttsConfiguration)
         {
             textToSpeechConfiguration = ttsConfiguration;
         }
