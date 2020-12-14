577135f0fb8a414c0e2db1002d8ba2a49832b847
Simon L <simon.lerch@innoactive.de>
2020-12-14 10:33:37 +0100

    fix: fixed the lag in the settings page (#39)

--- Editor/ProjectSettings/TextToSpeechSettingsProvider.cs
+++ Editor/ProjectSettings/TextToSpeechSettingsProvider.cs
@@ -4,13 +4,13 @@ using Innoactive.CreatorEditor.UI;
 using UnityEditor;
 using UnityEngine;
 
-internal class TextToSpeechSettingsProvider : SettingsProvider
+internal class TextToSpeechSettingsProvider : BaseSettingsProvider
 {
     const string Path = "Project/Creator/Text to Speech";
 
     public TextToSpeechSettingsProvider() : base(Path, SettingsScope.Project) {}
 
-    public override void OnGUI(string searchContext)
+    protected override void InternalDraw(string searchContext)
     {
         GUILayout.Label("Configuration for your Text to Speech provider.", CreatorEditorStyles.ApplyPadding(CreatorEditorStyles.Label, 0));
         
@@ -18,14 +18,18 @@ internal class TextToSpeechSettingsProvider : SettingsProvider
         
         TextToSpeechConfiguration config = TextToSpeechConfiguration.Instance;
         Editor.CreateEditor(config, typeof(TextToSpeechConfigurationEditor)).OnInspectorGUI();
-        if (EditorUtility.IsDirty(config))
-        {
-            config.Save();
-        }
-        
+
         GUILayout.Space(8);
         
         CreatorGUILayout.DrawLink("Need Help? Visit our documentation", "https://developers.innoactive.de/documentation/creator/latest/articles/developer/12-text-to-speech.html", 0);
+    }
+
+    public override void OnDeactivate()
+    {
+        if (EditorUtility.IsDirty(TextToSpeechConfiguration.Instance))
+        {
+            TextToSpeechConfiguration.Instance.Save();
+        }
     }
 
     [SettingsProvider]
